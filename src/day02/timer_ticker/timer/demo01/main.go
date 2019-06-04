a58455b992296edb1d5eb5ac4b0ea5b115b8d20e
wangxizzz <979675820@qq.com>
2019-06-04 16:33:19 +0800

    学完了基本的使用与语法

--- src/day02/timer_ticker/timer/demo01/main.go
+++ src/day02/timer_ticker/timer/demo01/main.go
@@ -12,7 +12,7 @@ func main() {
 		fmt.Printf("Expiration time : %v.\n", time.Now())
 		fmt.Printf("C`s len: %d\n", len(t.C))
 	}
-
+	defer t.Stop()
 	t = time.AfterFunc(1*time.Second, f)
 	//让当前Goroutine 睡眠2s，确保大于内容的完整
 	//这样做原因是，time.AfterFunc的调用不会被阻塞。它会以一部的方式在到期事件来临执行我们自定义函数f。
